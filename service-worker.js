1263fefc5c6ba025200714544f58ed32e9adbaa8
T <admin@new-crew.net>
2026-09-23 19:44:19 +0200

    emoji

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "meshcore-dashboard-v1.15.0";
+const CACHE_NAME = "meshcore-dashboard-v1.16.8";
 const APP_SHELL = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./version.json"];
 
 self.addEventListener("install", (event) => {
